419975e7e956888a09f5fec33cd3b150fee3a7b9
countz <marco.franzoni@gmail.com>
2013-03-08 17:26:51 +0100

    more flexibility

--- src/SHA2.java
+++ src/SHA2.java
@@ -5,40 +5,64 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 
-public class SHA2 {
+public class Digest {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		try {
-			MessageDigest md = MessageDigest.getInstance("SHA-256");
-			
-			String filename;
-			if(args.length == 1)
+
+
+
+			if(args.length == 2)
 			{
-				filename = args[0];
-				FileInputStream fis = new FileInputStream(filename);
-				
-				byte[] dataBytes = new byte[1024];
-				 
-		        int nread = 0;
-		        while ((nread = fis.read(dataBytes)) != -1) {
-		          md.update(dataBytes, 0, nread);
-		        };
-		        
-		        byte[] mdbytes = md.digest();
-		        
-		        StringBuilder sb = new StringBuilder();
-		        for (byte b : mdbytes) {
-		            sb.append(String.format("%02X ", b));
-		        }
-		        System.out.println("File name: " +filename);
-		        System.out.println("Digest SHA-256: " +sb.toString());
-			
+				if(args[0].equalsIgnoreCase("MD2") || args[0].equalsIgnoreCase("MD5") ||
+						args[0].equalsIgnoreCase("SHA-1") || args[0].equalsIgnoreCase("SHA-256") ||
+						args[0].equalsIgnoreCase("SHA-384") || args[0].equalsIgnoreCase("SHA-512") )
+				{
+					String algorithm = args[0];
+
+					MessageDigest md = MessageDigest.getInstance(algorithm);
+					String filename;
+					filename = args[1];
+					FileInputStream fis = new FileInputStream(filename);
+
+					byte[] dataBytes = new byte[1024];
+
+					int nread = 0;
+					while ((nread = fis.read(dataBytes)) != -1) {
+						md.update(dataBytes, 0, nread);
+					};
+					
+					fis.close();
+
+					byte[] mdbytes = md.digest();
+
+					StringBuilder sb = new StringBuilder();
+					for (byte b : mdbytes) {
+						sb.append(String.format("%02X ", b));
+					}
+					System.out.println("File name: " +filename);
+					System.out.println("Digest SHA-256: " +sb.toString());
+				}
+
+				else
+				{
+					System.out.println("Usage: java Digest [MD2|MD5|SHA-1|SHA-256|SHA-384|SHA-512] file");
+					System.exit(0);
+				}
+
+
+
 			}
-			
-			
+			else
+			{
+				System.out.println("Usage: java Digest [MD2|MD5|SHA-1|SHA-256|SHA-384|SHA-512] file");
+				System.exit(0);
+			}
+
+
 		} catch (NoSuchAlgorithmException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
